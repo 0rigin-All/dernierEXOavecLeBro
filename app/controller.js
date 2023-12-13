8255ae59eb432ea4c2b1a3980640a68919c9781b
0rigin-All <fabian.larwa.fl@gmail.com>
2023-12-13 14:55:49 +0100

    ajout timer

--- app/controller.js
+++ app/controller.js
@@ -3,8 +3,29 @@ async function noParam(req, res) {
 }
 
 const startGame = (req, res)=>{
+    let timer = 10; 
     const tab = [12,34,56,87,12,54]
     res.render('startGame', {tab, timer})
 }
+const timer = (req, res) => {
+    let timer = 10;
 
-module.exports = { noParam, startGame }
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+
+    const interval = setInterval(() => {
+        if (timer >= 0) {
+            res.write(`data: Timer: ${timer}\n\n`);
+            timer--;
+
+            if (timer < 0) {
+                clearInterval(interval);
+                res.write(`data: PERDU\n\n`);
+            }
+        }
+    }, 1000);
+}
+
+
+module.exports = { noParam, startGame, timer }
--- app/router.js
+++ app/router.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 const {noParam,
-    startGame
+    startGame,
+    timer
 } = require('./controller')
 
 router.get("/", noParam);
 router.get("/jeu", startGame)
-
+router.get('/timer', timer);
 module.exports = router
